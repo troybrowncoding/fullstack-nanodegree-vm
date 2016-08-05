0e60b1e398332b8b356b8f008452937a1efb101e
Troy Brown <troybrowncoding@gmail.com>
2016-08-05 06:55:56 -0400

    Fullstack P3
    Tournament Results

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -13,14 +13,31 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    #c.execute("DELETE FROM Matches;")
+    c.execute("UPDATE Players SET Wins = 0, Matches = 0;")
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM Players;")
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT COUNT(PlayerID) FROM Players;")
+    count = c.fetchone()
+    conn.close()
+    return count[0]
 
 
 def registerPlayer(name):
@@ -32,6 +49,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO Players (Player_Name) VALUES (%s)", (name,))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,6 +69,13 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT * FROM Players ORDER BY Wins DESC;")
+    standings = c.fetchall()
+    conn.commit()
+    conn.close()
+    return standings
 
 
 def reportMatch(winner, loser):
@@ -56,6 +85,24 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT * FROM Players WHERE PlayerID = %s", (winner,))
+    player = c.fetchone()
+    newwins = player[2]
+    newwins = newwins + 1
+    newmatches = player[3]
+    newmatches = newmatches + 1
+    c.execute("UPDATE Players SET Wins = %s, Matches = %s WHERE PlayerID = %s", (newwins, newmatches,winner))
+
+    c.execute("SELECT * FROM Players WHERE PlayerID = %s", (loser,))
+    player = c.fetchone()
+    newmatches = player[3]
+    newmatches = newmatches + 1
+    c.execute("UPDATE Players SET Matches = %s WHERE PlayerID = %s", (newmatches,loser))
+    
+    conn.commit()
+    conn.close()
  
  
 def swissPairings():
@@ -73,5 +120,21 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    players = countPlayers()
+    standings = playerStandings()
+    pairings = []
+    i = 0
+    paired = 1
 
-
+    while i < players:
+        if paired:
+            first = (standings[i][0], standings[i][1])
+            i = i + 1
+            paired = 0
+        else:
+            second = (standings[i][0], standings[i][1])
+            pair = first + second
+            pairings.append(pair)
+            i = i + 1
+            paired = 1
+    return pairings
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,21 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
 
+CREATE TABLE Players
+(
+	PlayerID serial PRIMARY KEY,
+	Player_Name varchar(255),
+	Wins int DEFAULT 0,
+	Matches int DEFAULT 0
+);
+
+--CREATE TABLE Matches
+--(
+--	MatchID serial PRIMARY KEY,
+--	Winner int,
+--	Loser int
+--);
